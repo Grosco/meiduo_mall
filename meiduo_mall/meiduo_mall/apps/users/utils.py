cc4f2efc92dbdaf92c98bb753b56f3d75981e392
Tom <tom@ifold.cf>
2022-10-07 00:26:58 +0800

    Customizing token claims

--- meiduo_mall/meiduo_mall/apps/users/utils.py
+++ meiduo_mall/meiduo_mall/apps/users/utils.py
@@ -2,9 +2,11 @@ import re
 
 from django.contrib.auth import get_user_model
 from django.contrib.auth.backends import ModelBackend
+from django.contrib.auth.models import update_last_login
 
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.settings import api_settings
 
 # from .models import User
 
@@ -17,9 +19,12 @@ class UsernameMobileAuthBackend(ModelBackend):
         is_admin = kwargs.get('is_admin', True)
 
         if is_admin:
-            user = self.get_user_by_account(account=username, is_staff=True)
+            # user = self.get_user_by_account(account=username, is_staff=True)
+            user = self.get_user_by_account(account=username, is_staff=False)
+            print('is_admin', user.username)
         else:
             user = self.get_user_by_account(account=username, is_staff=False)
+            print('is_not_amdin', user.username)
 
         if user and user.check_password(password):
             return user
@@ -51,22 +56,29 @@ class UsernameMobileAuthBackend(ModelBackend):
                 return user
 
 
+# start # Customizing token claims #
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
-    @classmethod
-    def get_token(cls, user):
-        # token = super().get_token(user)
-        token = super(MyTokenObtainPairSerializer, cls).get_token(user)
+    def validate(self, attrs):
+        data = super().validate(attrs)
 
-        # Add custom claims
-        token['username'] = user.username
-        # ...
+        refresh = self.get_token(self.user)
 
-        return token
+        data["refresh"] = str(refresh)
+        data["access"] = str(refresh.access_token)
+        data["id"] = str(self.user.id)
+        data["username"] = str(self.user.username)
+
+        if api_settings.UPDATE_LAST_LOGIN:
+            update_last_login(None, self.user)
+
+        return data
 
 
 class MyTokenObtainPairView(TokenObtainPairView):
     """
+    自定义jwt认证成功返回数据
     {"typ":"JWT","alg":"HS256"}
     {"token_type":"access","exp":1665052993,"iat":1665052963,"jti":"6531ea46700e441a8e12a4614cbefe14","user_id":21,"username":"mduser"}
     """
     serializer_class = MyTokenObtainPairSerializer
+# end # Customizing token claims #
